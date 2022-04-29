ed2a302918108f11d7879a3d12a0f6837989ed56
Radiobull <104358950+Radiobull@users.noreply.github.com>
2022-04-29 16:23:23 +0200

    Add files via upload

--- pizzeria.py
+++ pizzeria.py
@@ -1,67 +1,27 @@
 #classi per ragruppare varie propieta per le pizze e bevande
-class pizza1:
-    def __init__(self, nomeP, prezzoP, vegana):
+class Prd:
+    def __init__(self, nome, prezzo, vegana):
         
-        self.nomeP = nomeP
-        self.prezzoP = prezzoP
+        self.nome = nome
+        self.prezzo = prezzo
         self.vegana = vegana
 
-p1 = pizza1("Margherita", 4.50, True)
+p1 = Prd("Margherita", 4.50, "si")
 
+p2 = Prd("Patapizza", 6.50, "si") 
 
-class pizza2:
-    def __init__(self, nomeP, prezzoP, vegana):
+p3 = Prd("Pistacchiosa", 7.00, "no")
 
-        self.nomeP = nomeP
-        self.prezzoP = prezzoP
-        self.vegana = vegana
+p4 = Prd("Norma", 8.50, "si")
 
-p2 = pizza2("Patapizza", 6.50, True) 
+bevanda1 = Prd("Birra bionda (media)", 5.50, True)
+bevanda2 = Prd("Birra bionda (grande)", 7.00, True)
 
-class pizza3:
-    def __init__(self, nomeP, prezzoP, vegana):
+bevanda3 = Prd("Birra rossa (media)", 5.50, True)
+bevanda4 = Prd("Birra rossa (grande)", 7.00, True)
 
-        self.nomeP = nomeP
-        self.prezzoP = prezzoP
-        self.vegana = vegana
-
-p3 = pizza3("Pistacchiosa", 7.00, False)
-
-class pizza4:
-    def __init__(self, nomeP, prezzoP, vegana):
-
-        self.nomeP = nomeP
-        self.prezzoP = prezzoP
-        self.vegana = vegana
-
-p4 = pizza4("Norma", 8.50, True)
-
-class bevanda1:
-    def __init__(self, nomeB, prezzoB):
-
-        self.nomeB = nomeB
-        self.prezzoB = prezzoB
-
-bevanda1a = bevanda1("Birra bionda (media)", 5.50)
-bevanda1b = bevanda1("Birra bionda (grande)", 7.00)
-
-class bevanda2:
-    def __init__(self, nomeB, prezzoB):
-
-        self.nomeB = nomeB
-        self.prezzoB = prezzoB
-
-bevanda2a = bevanda2("Birra rossa (media)", 5.50)
-bevanda2b = bevanda2("Birra rossa (grande)", 7.00)
-
-class bevanda3:
-    def __init__(self, nomeB, prezzoB):
-
-        self.nomeB = nomeB
-        self.prezzoB = prezzoB
-
-bevanda3a = bevanda3("Bottiglietta d'acqua (naturale)", 2.00)
-bevanda3b = bevanda3("Bottiglietta d'acqua (frizzante)", 2.50)
+bevanda5 = Prd("Bottiglietta d'acqua 0,5 L (naturale)", 2.00, True)
+bevanda6 = Prd("Bottiglietta d'acqua 0,5 L (frizzante)", 2.50, True)
 
 #variabili
 totale = 0
@@ -77,9 +37,9 @@ while True:
     #il menù
     print("benvenuto gentile cliente, il menù è il seguente:\n")
     #il commando altro e le pizze
-    print("Altro:\n 0- vuoto\n \nPizze:\n", "1-", p1.nomeP, "vegana?:", p1.vegana, p1.prezzoP, "euro\n", "2-", p2.nomeP, "vegana?:", p2.vegana, p2.prezzoP, "euro\n", "3-", p3.nomeP, "vegana?:", p4.vegana, p3.prezzoP, "euro\n", "4-", p4.nomeP, "vegana?:", p4.vegana, p4.prezzoP, "euro\n")
+    print("Altro:\n 0- vuoto\n \nPizze:\n", "1-", p1.nome, "vegana?:", p1.vegana, p1.prezzo, "euro\n", "2-", p2.nome, "vegana?:", p2.vegana, p2.prezzo, "euro\n", "3-", p3.nome, "vegana?:", p3.vegana, p3.prezzo, "euro\n", "4-", p4.nome, "vegana?:", p4.vegana, p4.prezzo, "euro\n")
     #le bevande
-    print("Bevande:\n", "5-", bevanda1a.nomeB, bevanda1a.prezzoB, "euro\n", "6-", bevanda1b.nomeB, bevanda1b.prezzoB, "euro\n", "7-", bevanda2a.nomeB, bevanda2a.prezzoB, "euro\n", "8-", bevanda2b.nomeB, bevanda2b.prezzoB, "euro\n", "9-", bevanda3a.nomeB, bevanda3a.prezzoB,"euro\n", "10-", bevanda3b.nomeB, bevanda3b.prezzoB, "euro\n")
+    print("Bevande:\n", "5-", bevanda1.nome, bevanda1.prezzo, "euro\n", "6-", bevanda2.nome, bevanda2.prezzo, "euro\n", "7-", bevanda3.nome, bevanda3.prezzo, "euro\n", "8-", bevanda4.nome, bevanda4.prezzo, "euro\n", "9-", bevanda5.nome, bevanda5.prezzo,"euro\n", "10-", bevanda6.nome, bevanda6.prezzo, "euro\n")
 
     #qua provo a vedere se funziona col try ma se e minore di 0 la richiesta manda errore
     try:
@@ -106,25 +66,25 @@ while True:
             else:
                 if scelta == 0: pass
 
-                elif scelta == 1: totale = totale + p1.prezzoP
+                elif scelta == 1: totale +=p1.prezzo
 
-                elif scelta == 2: totale = totale + p2.prezzoP
+                elif scelta == 2: totale += p2.prezzo
 
-                elif scelta == 3: totale = totale + p3.prezzoP
+                elif scelta == 3: totale += p3.prezzo
 
-                elif scelta == 4: totale = totale + p4.prezzoP
+                elif scelta == 4: totale += p4.prezzo
 
-                elif scelta == 5: totale = totale + bevanda1a.prezzoB
+                elif scelta == 5: totale += bevanda1.prezzo
 
-                elif scelta == 6: totale = totale + bevanda1b.prezzoB
+                elif scelta == 6: totale += bevanda2.prezzo
 
-                elif scelta == 7: totale = totale + bevanda2a.prezzoB
+                elif scelta == 7: totale += bevanda3.prezzo
 
-                elif scelta == 8: totale = totale + bevanda2b.prezzoB
+                elif scelta == 8: totale += bevanda4.prezzo
 
-                elif scelta == 9: totale = totale + bevanda3a.prezzoB
+                elif scelta == 9: totale += bevanda5.prezzo
 
-                elif scelta == 10: totale = totale + bevanda3b.prezzoB
+                elif scelta == 10: totale += bevanda6.prezzo
 
                 else:
                     print(inputerror001)
@@ -149,7 +109,7 @@ while True:
     elif sceltaC == "no" or sceltaC == "NO" or sceltaC == "No" or sceltaC == "nO" or sceltaC == "n" or sceltaC == "N":
         print("programma arrestato")
         break
-        
+
     else:
         print(inputerror001)
         continue
